cdc51ec428693516b271067eb7fc0bea8963546a
thezrk181 <zaryabkhanloh@gmail.com>
2026-07-27 00:49:35 +0500

    feat: replace Splash Cursor with Magic Rings

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,7 +12,7 @@ import { useEffect, type ReactNode } from "react";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { CartProvider } from "../lib/cart-context";
-import SplashCursor from "../components/SplashCursor";
+import MagicRings from "../components/MagicRings";
 
 function NotFoundComponent() {
   return (
@@ -122,7 +122,31 @@ function RootComponent() {
   return (
     <QueryClientProvider client={queryClient}>
       <CartProvider>
-        <SplashCursor />
+        <div style={{ position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', zIndex: -1 }}>
+          <MagicRings
+            color="#fc42ff"
+            colorTwo="#42fcff"
+            ringCount={6}
+            speed={1}
+            attenuation={10}
+            lineThickness={2}
+            baseRadius={0.35}
+            radiusStep={0.1}
+            scaleRate={0.1}
+            opacity={0.6}
+            blur={0}
+            noiseAmount={0.1}
+            rotation={0}
+            ringGap={1.5}
+            fadeIn={0.7}
+            fadeOut={0.5}
+            followMouse={false}
+            mouseInfluence={0.2}
+            hoverScale={1.2}
+            parallax={0.05}
+            clickBurst={false}
+          />
+        </div>
         {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
         <Outlet />
       </CartProvider>
