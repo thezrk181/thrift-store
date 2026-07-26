cbc6e95776dd6dcd94c6f03abfd3744ca039e531
thezrk181 <zaryabkhanloh@gmail.com>
2026-07-27 00:41:48 +0500

    feat: add React Bits splash cursor animation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,6 +12,7 @@ import { useEffect, type ReactNode } from "react";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { CartProvider } from "../lib/cart-context";
+import SplashCursor from "../components/SplashCursor";
 
 function NotFoundComponent() {
   return (
@@ -121,6 +122,7 @@ function RootComponent() {
   return (
     <QueryClientProvider client={queryClient}>
       <CartProvider>
+        <SplashCursor />
         {/* Required: nested routes render here. Removing <Outlet /> breaks all child routes. */}
         <Outlet />
       </CartProvider>
