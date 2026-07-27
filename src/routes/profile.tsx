6fa2e4db0172eedbdae87aa9e09284a29a134e1c
thezrk181 <zaryabkhanloh@gmail.com>
2026-07-27 23:49:01 +0500

    fix: import Link in profile page

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, useNavigate, Link } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
 import { SiteNav } from "@/components/SiteNav";
 import { SiteFooter } from "@/components/SiteFooter";
